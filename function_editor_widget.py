7313ab6762907b93a8a662161169a7fddefd7a3e
Austin Elletson <elletson.austin@protonmail.com>
2026-01-12 18:18:54 -0600

    forgot this file

---
+++ function_editor_widget.py
@@ -0,0 +1,26 @@
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLineEdit, QLabel
+from PySide6.QtCore import Signal, Qt
+from scene import MathFunction
+
+class FunctionEditorWidget(QWidget):
+    equation_changed = Signal(MathFunction, str)
+
+    def __init__(self, math_function: MathFunction, parent=None):
+        super().__init__(parent)
+        self.math_function = math_function
+        
+        self.layout = QVBoxLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        
+        self.label = QLabel(f"f(x) = ")
+        self.equation_input = QLineEdit()
+        self.equation_input.setText(self.math_function.equation_str)
+        self.equation_input.returnPressed.connect(self.on_equation_changed)
+        
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.equation_input)
+
+    def on_equation_changed(self):
+        new_equation = self.equation_input.text()
+        if new_equation != self.math_function.equation_str:
+            self.equation_changed.emit(self.math_function, new_equation)
