ea7fdda1a79f6e7e27e40ee8e730f9cee713c170
Austin Elletson <elletson.austin@protonmail.com>
2026-01-19 11:26:17 -0600

    add file that i forgot to add

---
+++ render_types.py
@@ -0,0 +1,16 @@
+from enum import Enum, auto
+
+class Projection(Enum):
+    Perspective = auto()
+    Orthographic = auto()
+
+class SnapMode(Enum):
+    NONE = auto()
+    XY = auto()
+    XZ = auto()
+    YZ = auto()
+
+
+class CameraMode(Enum):
+    TwoD = auto()
+    ThreeD = auto()
